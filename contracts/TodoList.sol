9b89d2b2580fcaf42ab9022a52144904f8b982ec
Haoyang Liu <haoyang.liu@fdmgroup.com>
2022-07-30 21:54:05 -0400

    Updating...

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -15,8 +15,20 @@ contract TodoList {
     }
     mapping(uint256 => Task) public tasks;
 
+    event TaskCreated(uint256 id, string content, bool completed);
+
+    event TaskCompleted(uint256 id, bool completed);
+
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
