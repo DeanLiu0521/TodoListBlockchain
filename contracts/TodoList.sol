a77d9858629a57a998501d373b609efb009ece55
Haoyang Liu <haoyang.liu@fdmgroup.com>
2022-07-30 20:05:07 -0400

    updating

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,20 @@ pragma solidity >=0.5.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    constructor() public {
+        createTask("Check out my website.");
+    }
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+    mapping(uint256 => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
